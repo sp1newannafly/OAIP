1162e895f046991e330758e6320c9db29f6bda19
Maks <maks.prokopenkoo@yandex.com>
2021-12-01 14:54:45 +0300

    13lab

---
+++ 13.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand((int)time(0));
+    const int n = 10;
+    double a[n];
+
+    cout << "Matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 19 - 9;
+        cout << a[i] << " ";
+    }
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
+
+    cout << "\nSorted matrix:\n";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ 13.2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+using namespace std;
+void shellsort(int x[], int n)
+{
+	int i, j, k, с, t;
+	int aa = 0, bb = 0; с = n / 2;
+	while (с > 0)
+	{
+		for (i = 0; i < с; i++)
+		{
+			for (j = 0; j < n; j += с)
+			{
+				t = x[j];
+				for (k = j - с; k >= 0 && t < x[k]; k -= с)
+				{
+					bb++;
+					aa++;
+					x[k + с] = x[k];
+				}
+				x[k + с] = t;
+				aa++;
+			}
+		}
+		bb++;
+		if (с / 2 != 0)
+			с = с / 2;
+		else if (с == 1)
+			с = 0;
+		else
+			с = 1;
+	}
+}
+
+int main()
+{
+	int j;
+	j = 0;
+	setlocale(0, "ru");
+	int a[100]{};
+	int n;
+	int b[50]{};
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		a[i] = rand() % 50;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (a[i] % 2 == 0)
+			b[j++] = a[i];
+		else if (i % 2 != 0)
+			b[j++] = a[i];
+	}
+	cout << "Массив c числами oт 0 до 1000: ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << "  ";
+	}
+	cout << "\nМассив заполненный четными элементами:  ";
+	for (int i = 0; i < j; i++) {
+
+		cout << b[i] << " ";
+	}
+	cout << endl;
+	shellsort(b, j);
+	for (int i = 0; i < j; i++) {
+
+		cout << b[i] << " ";
+	}
+	return 0;
+}
