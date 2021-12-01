dd0a9365b351934044e474ff360ea45a9ca661c8
Maks <maks.prokopenkoo@yandex.com>
2021-12-01 14:54:16 +0300

    11lab

---
+++ 11.1.cpp
@@ -0,0 +1,22 @@
+﻿#include<fstream> 
+#include <iostream> 
+using namespace std;
+int main() {
+	setlocale(0, "Russian");
+	ofstream outfile("имя.txt");
+	char a[100], b[100], c[100], d[100];
+	char x = 1; do
+	{
+		cout << "Введите свою Фамилию : " << endl; cin >> a;
+		cout << "Введите свое Имя :" << endl; cin >> b;
+		cout << "Введите свое Отчетсво :" << endl; cin >> c;
+		cout << "Введите ваш рабочий номер :" << endl; cin >> d;
+		cout << a << "\n" << b << "\n" << c << "\n" << d << endl; cout << "Желаете отрекдактировать? 1-да или 0-нет!" << endl; cin >> x;
+	} while (x == '1');
+	outfile << a << "\n" << b << "\n" << c << "\n" << d << endl; ifstream input("имя.txt");
+	cout << "Фамилия:" << a << "\n" << endl; cout << "Имя:" << b << "\n" << endl;
+	cout << "Отчество:" << c << "\n" << endl;
+
+	cout << "Номер работника: " << d << "\n" << endl;
+	return 0;
+}
---
+++ 11.2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ofstream word("spisok.txt");
+    ifstream ford("spisok.txt");
+    setlocale(0, "Russian");
+    int a;
+    string xx, zz, yy, dd, ff;
+    string line;
+    char fia[300], pol[300], voz[300], mes[300], diagn[300], f[300], n[300], q[500];
+    int ui = 0;
+    do {
+        cout << "\t\t__________________________________________________________________________\n";
+        cout << "\t\t1. добавить запись \n";
+        cout << "\t\t2. вывести на экран данные массива \n";
+        cout << "\t\t3. вывести на экран результат выполнения массива\n";
+        cout << "\t\t4. End \n";
+        cout << "\t\t__________________________________________________________________________\n";
+        cin >> a;
+        switch (a) {
+        case 1:
+            cout << "ФИО клиента: "; // ввод данных происходит через нижнее подчеркивание.
+            cin >> fia;
+            cout << "Пол: ";
+            cin >> pol;
+            cout << "Возраст пациента: ";
+            cin >> voz;
+            cout << "Место жительства: ";
+            cin >> mes;
+            cout << "Диагноз: ";
+            cin >> diagn;
+            word << fia << ' ' << pol << ' ' << voz << ' ' << mes << ' ' << diagn;
+            word << endl;
+            break;
+        case 2:
+            cout << "ФИО клиента" << setw(20) << "Пол" << setw(20) << "Возраст пациента" << setw(20) << "Место жительства" << setw(20) << "Диагноз";
+            cout << endl;
+            while (!ford.eof())
+            {
+                ford >> fia >> pol >> voz >> mes >> diagn;
+                cout << fia << setw(20) << pol << setw(20) << voz << setw(20) << mes << setw(20) << diagn << endl;
+            }
+            break;
+
+        case 3:
+
+            while (getline(ford, line))
+            {
+                istringstream aaa(line);
+                aaa >> xx >> zz >> yy >> dd >> ff;
+
+                if ("Moscow" != xx)
+                {
+                    cout << "  " << xx;
+
+
+                }
+
+            }
+            break;
+        case 4:
+            word.close();
+            ford.close();
+            exit(0);
+            break;
+        }
+    } while (true);
+    return 0;
+    system("pause");
+}
