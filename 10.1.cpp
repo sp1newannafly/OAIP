2a9ae4ed5cdec6211656a6a630b64723531066e6
Maks <maks.prokopenkoo@yandex.com>
2021-12-01 14:51:53 +0300

    10lab

---
+++ 10.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct employee {
+    int num;
+    float salary;
+};
+
+int main(int argc, char* argv[])
+
+{
+    setlocale(0, "Russian");
+    employee arr[3];
+    for (int i = 0; i < 3; ++i) {
+        cout << "Введите данные " << i + 1 << " сотрудника : ";
+        cin >> arr[i].salary;
+        arr[i].num = i + 1;
+    }
+    cout << "\n Данные сотрудников : \n";
+    for (int i = 0; i < 3; ++i) {
+        cout << arr[i].num << ") " << arr[i].salary << "\n";
+    }
+
+    cout << endl;
+    return 0;
+}
---
+++ 10.2.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+struct date
+{
+    int Monat;
+    int Tag;
+    int Jahr;
+};
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    date sp1, sp2, sp3;
+    char dummy;
+    cout << "ввести день, месяц и год в формате 31/12/2002 ";
+    cin >> sp1.Tag >> dummy >> sp2.Monat >> dummy >> sp3.Jahr;
+    cout << sp1.Tag << dummy << sp2.Monat << dummy << sp3.Jahr;
+    return 0;
+}
