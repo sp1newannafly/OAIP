61797045ce53483a63157d02ef2c8db562fbe0c8
Maks <maks.prokopenkoo@yandex.com>
2022-02-28 11:53:26 +0300

    19lab

---
+++ 19.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b;
+    cout << "enter a:\n";
+    cin >> a;
+    cout << "enter b:\n";
+    cin >> b;
+
+    double* aPtr = &a;
+    double* bPtr = &b;
+    {
+        *aPtr *= 3;
+        *bPtr *= 3;
+    }
+    cout << "Результат А увелечения на 3 : " << a << endl;
+    cout << "Результат B увелечения на 3: " << b << endl;
+    swap(a, b);
+    cout << "aw: " << a << endl;
+    cout << "b: " << b << endl;
+    return 0;
+}
